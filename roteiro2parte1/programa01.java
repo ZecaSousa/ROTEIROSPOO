15fb7a401c30d582ea4f2420dbab498893e8bfec
ZecaSousa <163866006+ZecaSousa@users.noreply.github.com>
2024-04-11 13:59:41 -0300

    Create programa01.java

---
+++ roteiro2parte1/programa01.java
@@ -0,0 +1,18 @@
+package roteiro2parte1;
+
+public class programa01 {
+    public static void main(String[] args) {
+        int idade = 18;
+        char letra = 'a';
+        float numeroFloat = 3.14159f;
+        double numerodouble = 2.71828;
+        String nome = "zeca sousa";
+
+        System.out.println("exibir : " + numeroFloat);
+        System.out.println("sua idade corresponde a :" + idade);
+        System.out.println("seu cadastro e do tipo : " + letra);
+        System.out.println("sua nota foi de : " + numerodouble);
+        System.out.println("seu nome e: " + nome);
+    }
+
+}
